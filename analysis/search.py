8a8704fe3f68de155e159b37d5b79d48fe8c3c50
sahaavi <avisheksaha123@gmail.com>
2022-12-08 12:05:44 -0800

    update

--- analysis/search.py
+++ analysis/search.py
@@ -6,10 +6,12 @@ class Search(object):
         self.statement = df
 
     def search_category(self, cname):
-        print(self.statement.head())
-        return self.statement.head()
+        #print(self.statement.head())
+        matched_cat = self.statement.loc[self.statement['category'] == cname]
+        return matched_cat
 
 
 statement = pd.read_csv("analysis/statement.csv")
+cname_u = input("Enter the category name: ")
 p = Search(statement)
-p.search_category("sd")
+p.search_category("grocery")
