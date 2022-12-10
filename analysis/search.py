029a9dd874b3738170f29d137400fb752d827fbe
sahaavi <avisheksaha123@gmail.com>
2022-12-09 21:33:34 -0800

    search based on date created

--- analysis/search.py
+++ analysis/search.py
@@ -1,19 +1,37 @@
-import analysis.timerange as tr
+from datetime import datetime as dtime
 
 class Search(object):
 
     def __init__(self, df):
         self.statement = df
         
+    def search_date(self, date):
+        matched_date = self.statement.loc[self.statement['date'] == date]
+        return matched_date       
+
+    def search_range(self, start_date, end_date):
+        if(dtime.strptime(start_date, "%d/%m/%Y") < dtime.strptime(end_date, "%d/%m/%Y")):
+            matched_date = self.statement.loc[self.statement['date'].between(start_date, end_date)]
+            return matched_date
+        else:
+            return f"Starting date is bigger than ending date!"
+
+    #def search_month(self, month):
+
     def search_category(self, cname):
         matched_cat = self.statement.loc[self.statement['category'] == cname]
         return matched_cat
 
     def search_category(self, cname, start_date, end_date):
-        filtered_statement = tr.getrange(self.statement, start_date, end_date)
+        filtered_statement = self.statement.loc[self.statement['date'].between(start_date, end_date)]
         matched_cat = filtered_statement.loc[filtered_statement["category"] == cname]
         return matched_cat
 
     def search_amount(self, amount):
         matched_amt = self.statement.loc[self.statement['amount'] == amount]
+        return matched_amt
+
+    def search_amount(self, amount, start_date, end_date):
+        filtered_statement = self.statement.loc[self.statement['date'].between(start_date, end_date)]
+        matched_amt = filtered_statement.loc[filtered_statement['amount'] == amount]
         return matched_amt
--- analysis/timerange.py
+++ analysis/timerange.py
@@ -1,3 +1,12 @@
+def getday(df, day):
+
+
+def getmonth():
+
+
+def getyear():
+%Y-%m-%d %H:%M:%S%z
+
 def getrange(df, start_date, end_date):
         # Select DataFrame Rows between two dates
         mask = (df['date'] > start_date) & (df['date'] <= end_date)
