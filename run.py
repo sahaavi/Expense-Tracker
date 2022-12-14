fd365fd768c79cfd633c15597117599997ede0a5
sahaavi <avisheksaha123@gmail.com>
2022-12-14 01:07:17 -0800

    restore run with search & analysis

--- run.py
+++ run.py
@@ -1,9 +1,10 @@
-import data.store_data as sd
-import data.categorize_data as cd
+import pandas as pd
+
 from analysis.search import Search as s
 from analysis.analysis import Analysis as a
 
 def main():
+
     while True:
         print("1. Import/Export the data or add expenses.")
         print("2. Categorize the data")
@@ -12,66 +13,17 @@ def main():
         print("Type exit to exit")
         user_input = input("Choose an option: ")
 
-        df = base_df.show_expenses()
         if user_input == "1":
-            input1 = None
-            while input1 != "6":
-                print("1: add csv file")
-                print("2: add individual expense")
-                print("3: show expenses")
-                print("4: delete expense")
-                print("5: export expenses to csv")
-                print("6: exit")
-                input1 = input("Choose an option: ")
-                if input1 == "1":
-                    filename = input("Enter csv file name to add: ")
-                    base_df.add_csv(filename)
-                elif input1 == "2":
-                    user_date = input("Enter the date (MM/DD/YYYY): ")
-                    user_shopname = input("Enter the transaction name: ").upper()
-                    user_amount = float(input("Enter the amount: "))
-                    print(catlist)
-                    user_category = catlist.get(int(input("Enter a category: ")))
-                    base_df.add_expenses()
-                elif input1 == "3":
-                    print(base_df.show_expenses())
-                elif input1 == "4":
-                    print(base_df.show_expenses())
-                    whichrow = int(input("Which row number you would like to delete: "))
-                    base_df.delete_expenses(whichrow)
-                elif input1 == "5":
-                    newfilename = input("Name the exported file: ")
-                    base_df.export_expenses(newfilename)
-                elif input1 == "6":
-                    break
-                else:
-                    print("Input a valid choice (1-6)")
+            statement = pd.read_csv("statement.csv")
+            statement['date'] = pd.to_datetime(statement['date'])
+            p = s(statement)
+            print(p)
 
         elif user_input == "2":
-            input2 = None
-            while input2 != "5":
-                print("1: categorize all uncagetorized expenses")
-                print("2: categorize specific expense")
-                print("3: update a category name")
-                print("4: add a category")
-                print("5: exit")
-                input2 = input("Choose an option: ")
-                if input2 == "1":
-                    cd.categorize_all(df, catlist, dict_cat_shop)
-                elif input2 == "2":
-                    cd.categorize_item(df, catlist, dict_cat_shop)
-                elif input2 == "3":
-                    cd.update_category(df, catlist, dict_cat_shop)
-                elif input2 == "4":
-                    cd.add_category(catlist)
-                elif input2 == "5":
-                    break
-                else:
-                    print("Input a valid choice (1-5)")
+            print("nothing")
                     
         elif user_input == "3":
             while True:
-                print("What you want to do?")
                 print("1. Search a specific day's transactions")
                 print("2. Search transactions from a range of days")
                 print("3. Search a month's transactions")
@@ -113,9 +65,33 @@ def main():
                     print(p.search_category_range(amount, start_date, end_date))
                 elif user_input_s == "0":
                     break
+                else:
+                    print("Input a valid choice (0-8)")
 
         elif user_input == "4":
-            print("Under maintainance")
+            start_date = input("Enter start date (dd/mm/yyyy): ")
+            end_date = input("Enter end date (dd/mm/yyyy): ")
+            j = a(statement, start_date, end_date)
+            while True:
+                print("1. Total Expense")
+                print("2. Income Expense Ratio from a range of days")
+                print("3. Category Percentage")
+                print("4. Category Average")
+                print("0. Back to main")
+                user_input_s = input("Choose an option: ")
+                if user_input_s == "1":
+                    print(j.total_expense(statement))
+                if user_input_s == "2":
+                    income = float(input("Enter the income: "))
+                    print(j.income_expense_ratio_range(income))
+                elif user_input_s == "3":
+                    print(j.category_average())
+                elif user_input_s == "4":
+                    print(j.category_average())
+                elif user_input_s == "0":
+                    break
+                else:
+                    print("Input a valid choice (0-4)")
 
         elif user_input == "exit":
             break
