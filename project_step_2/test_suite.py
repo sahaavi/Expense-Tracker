e241cd1116438ab8485e0fad8600f57b444c4857
sahaavi <avisheksaha123@gmail.com>
2022-12-17 01:51:50 -0800

    unittest submit

---
+++ project_step_2/test_suite.py
@@ -0,0 +1,19 @@
+import unittest
+
+from test_search import TestSearch 
+from test_analysis import TestAnalysis 
+from test_store_data import TestStoreData
+from test_categorize import TestCategorize
+
+def my_suite():
+    suite = unittest.TestSuite() 
+    result = unittest.TestResult()
+    suite.addTest(unittest.makeSuite(TestSearch))
+    suite.addTest(unittest.makeSuite(TestAnalysis))  
+    suite.addTest(unittest.makeSuite(TestStoreData))
+    suite.addTest(unittest.makeSuite(TestCategorize))
+    runner = unittest.TextTestRunner() 
+    print(runner.run(suite))
+    #print(result)
+
+my_suite()
